08580459f1172c1fa51890f9b3d0b7a660ca766b
NazarChopko <tuskar24@gmail.com>
2025-04-19 16:12:40 +0300

    bugFix with files path

--- src/widgets/Booking/Booking.tsx
+++ src/widgets/Booking/Booking.tsx
@@ -2,7 +2,7 @@ import Button from '../../shared/Button/Button';
 import BookingInfo from '../../shared/BookingInfo';
 import CustomDatePicker from '../../shared/CustomDatePicker';
 import ProgressTool from '../../shared/ProgressTool';
-import Container from '../../layouts/Container/Container';
+import Container from '../../layouts/Container';
 
 import useHandleBooking from '../../hooks/useHandleBooking';
 
