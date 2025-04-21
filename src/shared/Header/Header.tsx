89be31f9f0096affec8de5f874d192b6bf17ea10
NazarChopko <tuskar24@gmail.com>
2025-04-21 14:04:07 +0300

    deleted dark prefix

--- src/shared/Header/Header.tsx
+++ src/shared/Header/Header.tsx
@@ -5,7 +5,7 @@ const Header = () => {
         <nav className="bg-white border-gray-200 px-4 lg:px-6 py-2.5 dark:bg-gray-800 ">
           <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
             <a href="#" className="flex items-center gap-1">
-              <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white text-transparent bg-clip-text bg-gradient-to-r from-primary to-[#86b1eb]">
+              <span className="self-center text-xl font-semibold whitespace-nowrap text-transparent bg-clip-text bg-gradient-to-r from-primary to-[#86b1eb]">
                 BooKing
               </span>
               <img src="/sea.png" className="w-6 h-6" />
@@ -13,7 +13,7 @@ const Header = () => {
             <div className="flex items-center lg:order-2 ">
               <a
                 href="#"
-                className="text-gray-800 hover:opacity-70 hidden lg:block dark:text-white hover:bg-gray-50 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 dark:hover:bg-gray-700 focus:outline-none dark:focus:ring-gray-800"
+                className="text-gray-800 hover:opacity-70 hidden lg:block  focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 hover:bg-gray-700 focus:outline-none"
               >
                 Log in
               </a>
@@ -26,7 +26,7 @@ const Header = () => {
               <button
                 data-collapse-toggle="mobile-menu-2"
                 type="button"
-                className="inline-flex items-center p-2 ml-1 text-sm text-gray-500 rounded-lg lg:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+                className="inline-flex items-center p-2 ml-1 text-sm text-gray-500 rounded-lg lg:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 "
                 aria-controls="mobile-menu-2"
                 aria-expanded="false"
               >
